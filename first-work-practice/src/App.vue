a1fc64fac450be0cf6e35d37b36bfae2cf627658
sjh9391985 <sjh9391985@gmail.com>
2024-08-15 13:47:14 +0900

    firt vue practice

---
+++ first-work-practice/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4>XX 원룸</h4>
+    <p>XX 만원</p>
+  </div>
+  <div>
+    <h4>XX 원룸</h4>
+    <p>XX 만원</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
