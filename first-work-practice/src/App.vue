a87f143bab42756027ab8c80410ee8ed7d5c2080
sjh9391985 <sjh9391985@gmail.com>
2024-08-15 15:43:52 +0900

    feat: '데이터 바인딩 학습'

--- first-work-practice/src/App.vue
+++ first-work-practice/src/App.vue
@@ -1,24 +1,37 @@
 <template>
+  <div class="black-bg" v-if="isOpen">
+    <div class="white-bg">
+      <h4>상세 페이지</h4>
+      <p>상세 페이지 내용</p>
+      <button @click="close">닫기</button>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, index) in menus" :key="index">{{ menu }}</a>
   </div>
-  <img alt="Vue logo" src="./assets/logo.png" />
   <div>
     <!-- product와 price를 함께 출력 -->
-    <div v-for="(product, index) in products" :key="index">
-      <h4>{{ product }}</h4>
-      <p>{{ prices[index] }} 만원</p>
+    <div v-for="(roomInfo, index) in rooms" :key="index">
+      <img :src="roomInfo.image" class="room-img" />
+      <h4 @click="this.isOpen = true">{{ roomInfo.title }}</h4>
+      <h5>{{ roomInfo.content }}</h5>
+      <p>{{ roomInfo.price }} 원</p>
       <button @click="increase(index)">허위매물신고</button> <span>신고수 : {{ counts[index] }}</span>
     </div>
   </div>
 </template>
 
 <script>
+import data from './data/oneroom.js';
+
 export default {
   name: 'App',
   data() {
     return {
       // 데이터 보관함
+      rooms: data,
+      isOpen: false,
       products: ['역삼동원룸', '천호동원룸', '마포구원룸'],
       prices: [30, 40, 50],
       menus: ['HOME', 'SHOP', 'ABOUT'],
@@ -29,19 +42,49 @@ export default {
     increase(i) {
       this.counts[i]++;
     },
+    close() {
+      this.isOpen = false;
+    },
   },
   components: {},
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: 0, 0, 0, 0.5;
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+.room-img {
+  width: 70%;
+  margin-top: 40px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-left: 20%;
+  margin-right: 20%;
 }
 
 .menu {
