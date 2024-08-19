58c861a37df16e02a5b41c661fbcf104064302e9
sjh9391985 <sjh9391985@gmail.com>
2024-08-19 19:39:02 +0900

    feat: 수정, 삭제, 추가 테스트

--- client-practice/src/views/HomeView.vue
+++ client-practice/src/views/HomeView.vue
@@ -7,12 +7,17 @@
 
 <script>
 import axios from 'axios';
+
+const uForm = { no: 1, name: '손재현', email: 'sjh9391985@gmail.com', pwd: '1234', gender: true };
+// const params = {
+//   params: { no: 1 },
+// };
 export default {
   name: 'HomeView',
   methods: {
     getData() {
       axios
-        .post('http://localhost:8080/findAll')
+        .put('http://localhost:8080/save', uForm)
         .then((response) => {
           console.log('response: ', response);
         })
